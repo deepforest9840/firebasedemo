a35202aae5f4091dd98324cd07242fc2f35cb0b3
deepforest9840 <sakib.cse.du@gmail.com>
2023-08-25 20:27:47 +0600

    Revert "third dlfjdlsfh"
    This reverts commit c27baae0e5799af4c4a15e7930e0d17e37549614.

--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -11,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("fdlsfjdsl");
-
-        System.out.println("ldsfjdlsjflsdkfjedofewr");
     }
 }
